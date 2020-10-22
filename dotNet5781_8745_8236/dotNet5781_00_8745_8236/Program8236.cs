235068955f675f76057d25133809e81cd5ed89c5
Itamar Cohen <itamar8236@gmail.com>
2020-10-23 01:32:22 +0300

    I added new Class

---
+++ dotNet5781_8745_8236/dotNet5781_00_8745_8236/Program8236.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8745_8236
+{
+    partial class Program
+    {
+        static partial void Welcome8236()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
