8b349404856abbb23747c8d8bba804b436268352
Avraham Glasberg <aglasber@g.jct.ac.il>
2020-10-23 00:05:17 +0300

    Initial code + Welcome8745

---
+++ dotNet5781_8745_8236/dotNet5781_00_8745_8236/Program8745.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8745_8236
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8745();
+            Welcome8236();
+            Console.ReadKey();
+        }
+        static partial void Welcome8236();
+
+        private static void Welcome8745()
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
