c7ce26607869f0466fc3a86917fbb7a955251939
Avraham Glasberg <aglasber@g.jct.ac.il>
2020-11-01 20:53:37 +0200

    bakbdskfa

---
+++ dotNet5781_8745_8236/dotNet5781_01_8745_8236/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_01_8745_8236
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
