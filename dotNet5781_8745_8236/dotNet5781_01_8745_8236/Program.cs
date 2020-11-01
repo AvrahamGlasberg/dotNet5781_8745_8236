a203e81287725c6df831340819ae2490b0c7f3d5
Itamar Cohen <itamar8236@gmail.com>
2020-11-01 21:54:24 +0200

    h

--- dotNet5781_8745_8236/dotNet5781_01_8745_8236/Program.cs
+++ dotNet5781_8745_8236/dotNet5781_01_8745_8236/Program.cs
@@ -10,7 +10,28 @@ namespace dotNet5781_01_8745_8236
     {
         static void Main(string[] args)
         {
-
+            char ch;
+            do
+            {
+                Console.WriteLine("For adding new bus, enter a. \nFor select bus for a ride enter r. \nFor treatment or refueling enter t. \nFor printing details for all buses since last treatment enter p. \nFor exit enter e. ");
+                char.TryParse(Console.ReadLine(), out ch);
+                switch (ch)
+                {
+                    case 'a':
+                        break;
+                    case 'r':
+                        break;
+                    case 't':
+                        break;
+                    case 'p':
+                        break;
+                    case 'e':
+                        break;
+                    default:
+                        Console.WriteLine("Illegal Input");
+                        break;
+                }
+            } while (ch != 'e');
         }
     }
 }
