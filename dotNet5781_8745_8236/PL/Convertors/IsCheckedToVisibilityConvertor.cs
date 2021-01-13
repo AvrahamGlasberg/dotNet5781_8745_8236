d06cb8fdd9cbd2ed20e9d5c9d2ef4a4e8cc3e8f4
Itamar Cohen <itamar8236@gmail.com>
2021-01-13 19:55:12 +0200

    dfg

---
+++ dotNet5781_8745_8236/PL/Convertors/IsCheckedToVisibilityConvertor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+namespace PL
+{
+    class IsCheckedToVisibilityConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? IsChecked = value as bool?;
+            if (IsChecked == true)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //never used!.
+            return false;
+        }
+    }
+}
